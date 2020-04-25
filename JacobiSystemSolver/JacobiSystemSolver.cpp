942731b027a9d6c63224337bdbe7b0de34534c06
Konstantin.Nisht <konstantin.nisht@jetbrains.com>
2020-04-25 14:52:58 +0300

    [task 2] fixed incorrect Jacobi solver; add results

--- JacobiSystemSolver/JacobiSystemSolver.cpp
+++ JacobiSystemSolver/JacobiSystemSolver.cpp
@@ -6,31 +6,34 @@
 
 #define MAX_NORM 1e9f
 
-JacobiSystemSolver::JacobiSystemSolver(double eps) : mEps(eps) {}
+JacobiSystemSolver::JacobiSystemSolver(double eps, int maxIterations) : mEps(eps), mIterations(maxIterations) {}
 
 void JacobiSystemSolver::findSolution(Matrix const &coeffs, Line const &freeVars, Line &startApproach) {
     unsigned int lineSize = startApproach.size();
 
     Line tempApproach(lineSize);
     double norm;
-
+    int passedIterations = 0;
     do {
+        if (passedIterations >= mIterations) break;
+        ++passedIterations;
         for (int i = 0; i < lineSize; ++i) {
             tempApproach[i] = freeVars[i];
             for (int j = 0; j < lineSize; ++j) {
                 if (i != j) {
                     tempApproach[i] -= coeffs[i][j] * startApproach[j];
                 }
-                tempApproach[i] /= coeffs[i][i];
             }
+            tempApproach[i] /= coeffs[i][i];
         }
-        norm = fabs(startApproach[0] - tempApproach[0]);
+        norm = 0;
         for (int i = 0; i < lineSize; ++i) {
             norm = std::max(norm, fabs(startApproach[i] - tempApproach[i]));
             startApproach[i] = tempApproach[i];
         }
         if (norm > MAX_NORM) {
             std::cout << "Impossible to solve!\n";
+            break;
             norm = 0;
             for (auto & element : startApproach) {
                 element = 0;
