5be07206bf021d73e966215517500448d9730884
unknown <nagoarsen@mail.ru>
2020-05-02 18:24:37 +0300

    add input read

--- task5/src/system/solver/main/Main.java
+++ task5/src/system/solver/main/Main.java
@@ -3,29 +3,61 @@ package system.solver.main;
 import system.solver.util.Matrix;
 import system.solver.Solver;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    private static final String DOMINATED = "matrices/dominated";
+    public static final String HILBERT = "matrices/hilbert";
+    public static final String RANDOM = "matrices/random";
+
     public static void main(String[] args) {
-        List<List<Double>> table = new ArrayList<>() {{ // example matrix A
-            add(new ArrayList<>() {{ add(2.0); add(1.0); add(1.0); add(4.0); }});
-            add(new ArrayList<>() {{ add(1.0); add(-1.0); add(0.0); add(2.0); }});
-            add(new ArrayList<>() {{ add(1.0); add(0.0); add(2.0); add(5.0); }});
-            add(new ArrayList<>() {{ add(4.0); add(2.0); add(5.0); add(-2.0); }});
-        }};
+        List<List<Double>> A = new ArrayList<>();
+        List<List<Double>> b = new ArrayList<>();
+        Solver solver;
 
-        List<List<Double>> bTable = new ArrayList<>() {{ // example vector (which is considered as a matrix) b
-            add(new ArrayList<>() {{ add(2.0); }});
-            add(new ArrayList<>() {{ add(-1.0); }});
-            add(new ArrayList<>() {{ add(4.0); }});
-            add(new ArrayList<>() {{ add(3.0); }});
-        }};
+        readInput(DOMINATED, A, b);
 
-        Matrix A = new Matrix(table);
-        Matrix b = new Matrix(bTable);
+        solver = new Solver(new Matrix(A), new Matrix(b));
+        solver.solve();
 
-        Solver solver = new Solver(A, b);
-        solver.solve(); // solve Ax = b
+        readInput(HILBERT, A, b);
+
+        solver = new Solver(new Matrix(A), new Matrix(b));
+        solver.solve();
+
+        readInput(RANDOM, A, b);
+
+        solver = new Solver(new Matrix(A), new Matrix(b));
+        solver.solve();
+    }
+
+    private static void readInput(String fileName, List<List<Double>> A, List<List<Double>> b) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            int n = Integer.parseInt(reader.readLine());
+
+            for (int i = 0; i < n; i++) {
+                String line = reader.readLine();
+                Scanner scanner = new Scanner(line);
+
+                List<Double> row = new ArrayList<>();
+                for (int j = 0; j < n; j++) {
+                    row.add(scanner.nextDouble());
+                }
+                A.add(row);
+            }
+
+            String line = reader.readLine();
+            Scanner scanner = new Scanner(line);
+            for (int i = 0; i < n; i++) {
+                List<Double> row = new ArrayList<>();
+                row.add(scanner.nextDouble());
+                b.add(row);
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to read input from file \"" + fileName + "\"");
+        }
     }
 }
